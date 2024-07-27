c1a8929df0630143038f664e391422cdca79fc5a
Rudney <rundeyngueko@gmail.com>
2024-07-27 18:59:57 +0300

    implement product service 2

--- services/product/src/main/java/com/shop/product/controller/ProductController.java
+++ services/product/src/main/java/com/shop/product/controller/ProductController.java
@@ -1,14 +1,16 @@
 package com.shop.product.controller;
 
+import com.shop.product.payload.rq.ProductPurchaseRequest;
 import com.shop.product.payload.rq.ProductRequest;
+import com.shop.product.payload.rs.ProductPurchaseResponse;
+import com.shop.product.payload.rs.ProductResponse;
 import com.shop.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -21,5 +23,24 @@ public class ProductController {
             @RequestBody @Valid ProductRequest productRequest
     ){
         return ResponseEntity.ok(productService.createProduct(productRequest));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
+            @RequestBody List<ProductPurchaseRequest> purchaseRequests
+    ){
+        return ResponseEntity.ok(productService.purchaseProducts(purchaseRequests));
+    }
+
+    @GetMapping("/{product-id}")
+    public ResponseEntity<ProductResponse> findById(
+            @PathVariable("product-id") Integer productId
+    ){
+        return ResponseEntity.ok(productService.findById(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findAll(){
+        return ResponseEntity.ok(productService.findAll());
     }
 }
